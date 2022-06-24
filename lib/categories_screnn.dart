0fdf8c72ccc9edb32db06c31c4557604d098ec87
Terin Sebastin <terin01@gmail.com>
2022-06-24 03:32:31 +0100

    Created gridview for meals app

---
+++ lib/categories_screnn.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app_flutter/category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeals')),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES
+              .map((elm) => CategoryItem(title: elm.title, color: elm.color))
+              .toList()),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './categories_screnn.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,31 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('Navigation Time!'),
-      ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber),
+          canvasColor: const Color.fromRGBO(225, 225, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2:
+                  const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)))),
+      home: const CategoriesScreen(),
     );
   }
 }
