befed48dfe53f1093e61af6bcfdc9641cc9ef346
Terin Sebastin <terin01@gmail.com>
2022-06-29 03:29:05 +0100

    Finishing the meal List Item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -16,6 +16,44 @@ class MealItem extends StatelessWidget {
       required this.complexity,
       required this.affordability})
       : super(key: key);
+  // ignore: non_constant_identifier_names
+  String get ComplexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        // ignore: dead_code
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        // ignore: dead_code
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        // ignore: dead_code
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        // ignore: dead_code
+        break;
+      case Affordability.Luxurious:
+        return 'Expensive';
+        // ignore: dead_code
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        // ignore: dead_code
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
 
   void selectMeal() {}
   @override
@@ -41,8 +79,60 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  // ignore: sized_box_for_whitespace
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: const TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(ComplexityText)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityText)
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
